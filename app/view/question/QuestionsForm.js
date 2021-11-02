65e92d46e5a54ab0296e60ae79aa1608da040df3
Roseann28 <roseann2044@gmail.com>
2021-11-02 13:08:21 +0200

    Personal infomation card

--- app/view/question/QuestionsForm.js
+++ app/view/question/QuestionsForm.js
@@ -1,8 +1,172 @@
 Ext.define('SurveyApp.view.question.QuestionsForm',{
-    extend:'Ext.window.Window',
+    extend:'Ext.form.Panel',
+    layout:'card',
+    width:900,
+    xtype:'questionnaire',
+    store:'categories',
     controller:'questionsformcontroller',
+    viewModel:{
+        data:{
+            record:{
+                id:'Tell us about yourself',
+                id1:'When were you born?',
+                id2:'Can you sign a four year contract with Systech?',
+                id3:'if safaricom offered you a job, with a salary of 300,000 would you take it?Give a reason'
+            }
+            
+        }
+        
+    },
+    
+    items:[{
+        title:'Personal Information',
+        items: [{   
+            layout: 'accordion',
+                       
+            items: [
+                {
+                title: 'Question 1',
+                
+                items:[{
+                    bind:{
+                        html: '<h1>{record.id}</h1>',
+                    },
+                    
+                    
+                },{
+                    xtype:'textfield',
+                    hidden:true,
+                    bind:{
+                        value:'{record.id}'
+                    },
 
-    fields:[
+                },
+                {
+                    
+                    xtype:'htmleditor'
 
-    ],
+                }
+                    
+                    
+                ],
+            },
+                
+            {
+                title: 'Question 2',
+                items:[{
+                    bind:{
+                        html: '<h1>{record.id1}</h1>',
+                    },
+                    
+                    
+                },{
+                    xtype:'textfield',
+                    hidden:true,
+                    bind:{
+                        value:'{record.id1}'
+                    },
+
+                },{
+                    xtype:'dateformatfield', 
+                    reference:'dob',
+                    listeners:{
+                        select:'onDatePicked'
+                    }
+
+                },{
+                    xtype:'textfield',
+                    fieldLabel:'Age',
+                    reference:'age',
+                    //width:60,
+                    readOnly:true,
+                    listeners:{
+                        change:"onChange"    
+                   },
+                    
+
+                }],
+            },
+            {
+                title: 'Question 3',
+                header: {
+                    style: {
+                        backgroundColor: 'red',
+                        
+                    }
+                },
+                
+                items:[{
+                    hidden:true,
+                    reference:'systechPeriod',
+                    items:[{
+                        bind:{
+                            html: '<h1>{record.id2}</h1>',
+                        },
+                        
+                        
+                    },{
+                        xtype:'textfield',
+                        hidden:true,
+                        bind:{
+                            value:'{record.id2}'
+                        },
+    
+                    },
+                    {
+                        xtype: 'radiogroup',
+                        vertical: true,
+                        items: [
+                            { boxLabel: 'Yes', name: 'options', inputValue: 'Yes'},
+                            { boxLabel: 'No', name: 'options', inputValue: 'No'}
+                        ],
+                        listeners:{
+                            change:"differentQs",
+                            change:'onAnswered'
+                        }
+                    }]
+
+                }]
+                
+        },{
+            title: 'Question 4',
+            
+            items:[{
+                hidden:true,
+                reference:'differentOpportunity',
+                items:[{
+                    bind:{
+                        html: '<h1>{record.id3}</h1>',
+                    },
+                    
+                    
+                },{
+                    xtype:'textfield',
+                    hidden:true,
+                    bind:{
+                        value:'{record.id3}'
+                    },
+
+                },
+                {
+                    xtype: 'textareafield',
+        
+                }]
+
+            }]
+            
+    }],
+    }]
+}],
+        bbar:[
+            {
+                text:'Next',
+                handler:'onNextButton'
+            },
+            {
+                text:'Back',
+                handler:'onBackButton'
+            }
+        ]
+
+    
 });
--- app/view/question/QuestionsFormController.js
+++ app/view/question/QuestionsFormController.js
@@ -11,5 +11,37 @@ Ext.define('SurveyApp.view.question.QuestionsFormController',{
     },
     onReset:function(){
         //on rest function implemetation
-    }
+    },
+    onDatePicked:function( field, value, eOpts ){
+        var view=this.getView(),
+        agefield=view.lookupReference('age');
+        // var d=value.getValue();
+        // console.log(d);
+        agefield.setValue(Ext.Date.diff( value, new Date(), Ext.Date.YEAR ));
+        // console.log(value,Ext.Date.YEAR);
+        // agefield.setValue(value,Ext.Date.YEAR)
+        //agefield.setValue(Ext.Date.add(value, Ext.Date.YEAR, 10))
+    },
+    onChange:function(field, newValue, oldValue, eOpts) {
+        var view=this.getView();
+
+        if (newValue>=18) {
+            var period=view.lookupReference('systechPeriod');
+            period.setVisible(true);  
+        }
+        
+
+    },
+    differentQs:function(cmp, newValue, oldValue, eOpts){
+        var view=this.getView();
+        console.log(newValue.options);
+        console.log(cmp.getValue())
+        if(newValue.options=='Yes'){
+            var differentOpportunity=view.lookupReference('differentOpportunity');
+            differentOpportunity.setVisible(true);
+        }else{
+            console.log("nooooo")
+        }
+    },
+    
 })
